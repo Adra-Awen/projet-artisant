736e6601bf02e3b9acd3430ab9eaafb354d66814
Nolwenn Bauvais <bauvais.n@protonmail.com>
2026-03-23 17:10:37 +0100

    modify: app.js

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const categoriesRouter = require('./src/routes/categoryRoutes');
+const specialitiesRouter = require('./src/routes/specialityRoutes');
+const entreprisesRouter = require('./src/routes/entrepriseRoutes');
+// const contactRouter = require('./src/routes/contactRoutes');
 
-var app = express();
+const app = express();
 
 // DATABASE CONNECTION //
 const { sequelize } = require('./src/models');
@@ -23,14 +27,20 @@ sequelize.authenticate()
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// Middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/categorie', categoriesRouter);
+app.use('/specialitie', specialitiesRouter);
+app.use('/entreprise', entreprisesRouter);
+// app.use('/contacts', contactRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
